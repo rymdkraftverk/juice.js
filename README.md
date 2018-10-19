21fd68383454995f27919fef8f3105bc1892961b
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-10-19 10:00:38 +0200

    Add easeInOut and parabola functions

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # juice.js
 
-API
+All functions return a function with the signature `(time) => number`
 
-sine({ start, end, speed }) => (time) => coordinate
+The returned number is commonly used for setting the position of an element, but could be used for anything.
 
-TODO
+## API
 
-- [ ] Setup an examples page, using level1 to demonstrate all the functions
+easeInOut = ({
+  start, end, duration, startTime = 0,
+}) => (time) => number
+
+parabola = ({
+  start, end, offset, modifier = 1,
+}) => (time) => number
+
+sine = ({ start, end, speed }) => (time) => number
+
+## TODO
+
+- [ ] Setup an examples page, using pixi and level1 to demonstrate all the functions
 - [ ] Online editor where you can play around with values
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+const getNormalizer = (x1, x2) => (4 / ((x1 ** 2) + (x2 ** 2)))
+
+export const easeInOut = ({
+  start, end, duration, startTime = 0,
+}) => {
+  if (duration <= 0) {
+    error('easeInOut: duration has to be positive')
+  }
+  const endTime = startTime + duration
+  const speed = 4 * ((start - end) / ((startTime - endTime) ** 3))
+  const positionOffset = (end + start) / 2
+  const timeOffset = (endTime + startTime) / 2
+
+  return t => (speed * ((t - timeOffset) ** 3)) + positionOffset
+}
+
+export const parabola = ({
+  start, end, offset, modifier = 1,
+}) => (t) => {
+  const normalizer = getNormalizer(start, end)
+  return offset + ((Math.abs(start - end) * modifier) * (normalizer * (t - start) * (t - end)))
+}
+
 export const sine = ({
   start, end, speed,
 }) => (t) => {
