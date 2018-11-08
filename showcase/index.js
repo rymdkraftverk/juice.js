b9347d392a2bb9f27da83f5520d7ffd85d0fcb41
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-11-08 16:21:08 +0100

    Add react to the showcase app

--- showcase/index.js
+++
@@ -1,4 +0,0 @@
-// TODO
-// import * as l1 from 'l1';
-// import * as PIXI from 'pixi.js';
-// import * as juice from '..';
---
+++ showcase/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components/macro'
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
+
+ReactDOM.render(
+  <div>
+    <GlobalStyles />
+    <App />
+  </div>
+  ,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
