277a0c7c78e74af79219652fa3c413f997eeac31
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-04-17 10:51:23 +0900

    Update juice.js
     - Fix easeIn broken with negative endValue
     - Add endValue and duration to linear
     - Make 0 startValue for all functions
     - Add unit tests for all functions
     - Format code with xo and prettier

--- src/index.js
+++ src/index.js
@@ -1,78 +1,77 @@
-const getNormalizer = (x1, x2) => 4 / ((2 * x1 * x2) - (x1 ** 2) - (x2 ** 2));
+const getNormalizer = (x1, x2) => 4 / (2 * x1 * x2 - x1 ** 2 - x2 ** 2)
 
 export const easeInOut = ({
-  startValue, endValue, duration, startTime = 0,
+  endValue,
+  duration,
+  startValue = 0,
+  startTime = 0,
 }) => {
   if (duration <= 0) {
-    console.error('easeInOut: duration has to be positive');
+    console.error('easeInOut: duration has to be positive')
   }
-  const endTime = startTime + duration;
-  const speed = 4 * ((startValue - endValue) / ((startTime - endTime) ** 3));
-  const positionOffset = (endValue + startValue) / 2;
-  const timeOffset = (endTime + startTime) / 2;
 
-  return (t) => (speed * ((t - timeOffset) ** 3)) + positionOffset;
-};
+  const endTime = startTime + duration
+  const speed = 4 * ((startValue - endValue) / (startTime - endTime) ** 3)
+  const positionOffset = (endValue + startValue) / 2
+  const timeOffset = (endTime + startTime) / 2
 
-export const easeIn = ({
-  startValue,
-  endValue,
-  duration,
-}) => {
-  const speedParameter = Math.log(1 - startValue + endValue) / duration;
+  return (t) => speed * (t - timeOffset) ** 3 + positionOffset
+}
 
-  return (t) => (startValue - 1) + (Math.E ** (t * speedParameter));
-};
+export const easeIn = ({ endValue, duration, startValue = 0 }) => {
+  const speedParameter =
+    Math.log(1 - startValue + Math.abs(endValue)) / duration
 
-export const easeOut = ({
-  startValue,
-  endValue,
-  duration,
-}) => {
-  const functionHeight = startValue - endValue;
-  const speedParameter = Math.log(1 / 100) / duration;
+  const modifier = endValue < startValue ? -1 : 1
 
-  return (t) => endValue + functionHeight * (Math.E ** (t * speedParameter));
-};
+  return (t) => {
+    return (startValue - 1 + Math.E ** (t * speedParameter)) * modifier
+  }
+}
 
-export const linear = ({
-  startValue = 0,
-  speed = 1,
-}) => (t) => startValue + (t * speed);
+export const easeOut = ({ endValue, duration, startValue = 0 }) => {
+  const functionHeight = startValue - endValue
+  const speedParameter = Math.log(1 / 100) / duration
+
+  return (t) => endValue + functionHeight * Math.E ** (t * speedParameter)
+}
+
+export const linear = ({ endValue, duration, startValue = 0 }) => {
+  const velocity = (endValue - startValue) / duration
+  return (t) => t * velocity
+}
 
 export const parabola = ({
-  duration, height, startValue = 0, startTime = 0,
+  duration,
+  height,
+  startValue = 0,
+  startTime = 0,
 }) => (t) => {
-  const endTime = startTime + duration;
-  const normalizer = getNormalizer(startTime, endTime);
-  return startValue + (height * normalizer * (t - startTime) * (t - endTime));
-};
+  const endTime = startTime + duration
+  const normalizer = getNormalizer(startTime, endTime)
+  return startValue + height * normalizer * (t - startTime) * (t - endTime)
+}
 
-export const parabolaAngle = ({
-  start,
-  end,
-  modifier = 1,
-}) => (x) => {
-  const normalizer = getNormalizer(start, end);
-  return Math.atan((Math.abs(start - end) * modifier) * normalizer * ((2 * x) - start - end));
-};
+// export const parabolaAngle = ({ start, end, modifier = 1 }) => (x) => {
+//   const normalizer = getNormalizer(start, end)
+//   return Math.atan(
+//     Math.abs(start - end) * modifier * normalizer * (2 * x - start - end),
+//   )
+// }
 
 // TODO: where to start in the animation: phase shift
-export const sine = ({
-  startValue, endValue, duration,
-}) => (t) => {
-  const middle = ((startValue + endValue) / 2);
-  return middle + ((middle - startValue) * Math.sin((t * Math.PI * 2) / duration));
-};
+export const sine = ({ endValue, duration, startValue = 0 }) => (t) => {
+  const middle = (startValue + endValue) / 2
+  return middle + (middle - startValue) * Math.sin((t * Math.PI * 2) / duration)
+}
 
-// util functions
+// * Util functions
 
-export const offsetTime = (offset) => (f) => (t) => f(t - offset);
+export const offsetTime = (offset) => (f) => (t) => f(t - offset)
 
-export const limitLower = (limit) => (f) => (t) => Math.max(f(t), limit);
+export const limitLower = (limit) => (f) => (t) => Math.max(f(t), limit)
 
-export const limitUpper = (limit) => (f) => (t) => Math.min(f(t), limit);
+export const limitUpper = (limit) => (f) => (t) => Math.min(f(t), limit)
 
-export const glueFunctions = (limit) => (f) => (g) => (t) => (t < limit
-  ? f(t)
-  : g(t));
+export const glueFunctions = (limit) => (f) => (g) => (t) =>
+  t < limit ? f(t) : g(t)
---
+++ test.js
@@ -0,0 +1,160 @@
+import test from 'ava'
+import * as juice from './src'
+import { times } from 'lodash/fp'
+
+const duration = 5
+
+test('linear - positive endValue', (t) => {
+  const getX = juice.linear({
+    endValue: 40,
+    duration,
+  })
+  times((index) => {
+    t.is(getX(index), 8 * index)
+  }, duration)
+})
+
+test('linear - negative endValue', (t) => {
+  const getX = juice.linear({
+    endValue: -40,
+    duration,
+  })
+  times((index) => {
+    t.is(getX(index), -8 * index)
+  }, duration)
+})
+
+test('easeOut - positive endValue', (t) => {
+  const getX = juice.easeOut({
+    endValue: 40,
+    duration,
+  })
+  const expectedValues = [
+    0,
+    24.07571317786011,
+    33.66042723015555,
+    37.47617062207922,
+    38.99524542739617,
+  ]
+  times((index) => {
+    t.is(getX(index), expectedValues[index])
+  }, duration)
+})
+
+test('easeOut - negative endValue', (t) => {
+  const getX = juice.easeOut({
+    endValue: -40,
+    duration,
+  })
+  const expectedValues = [
+    0,
+    -24.07571317786011,
+    -33.66042723015555,
+    -37.47617062207922,
+    -38.99524542739617,
+  ]
+  times((index) => {
+    t.is(getX(index), expectedValues[index])
+  }, duration)
+})
+
+test('easeIn - positive endValue', (t) => {
+  const getX = juice.easeIn({
+    endValue: 40,
+    duration,
+  })
+  const expectedValues = [
+    0,
+    1.1016324782757847,
+    3.416859073743617,
+    8.282614481346688,
+    18.508644077311324,
+  ]
+  times((index) => {
+    t.is(getX(index), expectedValues[index])
+  }, duration)
+})
+
+test('easeIn - negative endValue', (t) => {
+  const getX = juice.easeIn({
+    endValue: -40,
+    duration,
+  })
+  const expectedValues = [
+    -0,
+    -1.1016324782757847,
+    -3.416859073743617,
+    -8.282614481346688,
+    -18.508644077311324,
+  ]
+  times((index) => {
+    t.is(getX(index), expectedValues[index])
+  }, duration)
+})
+
+test('parabola', (t) => {
+  const getX = juice.parabola({
+    height: 20,
+    duration,
+  })
+  const expectedValues = [0, 12.8, 19.200000000000003, 19.200000000000003, 12.8]
+  times((index) => {
+    t.is(getX(index), expectedValues[index])
+  }, duration)
+})
+
+test('easeInOut - positive endValue', (t) => {
+  const getX = juice.easeInOut({
+    endValue: 40,
+    duration,
+  })
+  const expectedValues = [0, 15.68, 19.84, 20.16, 24.32]
+  times((index) => {
+    t.is(getX(index), expectedValues[index])
+  }, duration)
+})
+
+test('easeInOut - negative endValue', (t) => {
+  const getX = juice.easeInOut({
+    endValue: -40,
+    duration,
+  })
+  const expectedValues = [0, -15.68, -19.84, -20.16, -24.32]
+  times((index) => {
+    t.is(getX(index), expectedValues[index])
+  }, duration)
+})
+
+test('sine - negative endValue', (t) => {
+  const getX = juice.sine({
+    endValue: 40,
+    duration,
+  })
+  const expectedValues = [
+    20,
+    39.02113032590307,
+    31.755705045849467,
+    8.24429495415054,
+    0.9788696740969272,
+  ]
+  times((index) => {
+    t.is(getX(index), expectedValues[index])
+  }, duration)
+})
+
+test('sine - positive endValue', (t) => {
+  const getX = juice.sine({
+    endValue: -40,
+    duration,
+  })
+  const expectedValues = [
+    -20,
+    -39.02113032590307,
+    -31.755705045849467,
+    -8.24429495415054,
+    -0.9788696740969272,
+  ]
+  times((index) => {
+    t.is(getX(index), expectedValues[index])
+  }, duration)
+})
