cd4193014b6f536c9b9eca14c7b4356cf1abb9d1
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-05-15 10:42:03 +0900

    Update showcase

--- showcase2/src/App.js
+++ showcase2/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import * as juice from 'juice.js'
 import _ from 'lodash/fp'
 import * as PIXI from 'pixi.js'
@@ -6,7 +6,6 @@ import styled from 'styled-components'
 import Size from './constant/size'
 import Color from './constant/color'
 import features from './constant/features'
-import Drop from './react-drop/Drop'
 
 const Title = styled.div`
   font-weight: bold;
@@ -18,9 +17,7 @@ const GITHUB_URL = 'https://github.com/rymdkraftverk/juice.js'
 
 const Canvas = styled.div``
 
-const Container = styled.div`
-  display: flex;
-`
+const Container = styled.div``
 
 const ControlPanel = styled.div`
   width: ${Size.LEFT_COLUMN_WIDTH}px;
@@ -39,6 +36,7 @@ const Logo = styled.div`
   align-items: center;
   width: ${Size.LEFT_COLUMN_WIDTH}px;
   background-color: ${Color.BLUE};
+  font-weight: bold;
   height: 100%;
 `
 
@@ -54,11 +52,56 @@ const Main = styled.div`
   display: flex;
 `
 
+const ParameterTitle = styled.div`
+  border: 1px solid blanchedalmond;
+  border-radius: 2px;
+  padding: 5px;
+  cursor: pointer;
+  user-select: none;
+`
+
+const Options = styled.div``
+
+const Option = styled.div`
+  padding: 5px;
+  cursor: pointer;
+  user-select: none;
+  background-color: ${({ selected }) =>
+    selected ? Color.BLUE : 'transparent'};
+  /* TODO: Hover effect */
+  /* TODO: Click effect */
+`
+
 let time = 0
 
+// TODO (parameters):
+// x, y, scale
+// Pig start in the middle?
+
+const START_X = 0
+
+let sprite
+
+const reset = () => {
+  sprite.x = START_X
+  time = 0
+}
+
 function App() {
-  const [x, setX] = useState(0)
   const [juicers, setJuicers] = useState([])
+  const [selectedFeature, setSelectedFeature] = useState(null)
+  const [configuration, setConfiguration] = useState({})
+  const updateFn = useRef()
+
+  useEffect(() => {
+    updateFn.current = () => {
+      juicers.forEach((juicer) => {
+        sprite.x = juicer.getValue(time)
+      })
+
+      time += 1
+    }
+  }, [juicers])
 
   useEffect(() => {
     const app = new PIXI.Application({
@@ -67,59 +110,100 @@ function App() {
       height: 600,
     })
     document.getElementById('canvas').appendChild(app.view)
-    const sprite = PIXI.Sprite.from('asset/pig.png')
+    sprite = PIXI.Sprite.from('asset/pig.png')
     app.stage.addChild(sprite)
-    app.ticker.add(() => {
-      // sprite.x += 1
-      juicers.forEach((juicer) => {
-        console.log('hello')
-        sprite.x = juicer.getValue(time)
-      })
 
-      time += 1
+    app.ticker.add(() => {
+      updateFn.current()
     })
   }, [])
+
   return (
-    <div>
+    <Container>
       <Header>
         <Logo>
-          <Title>juice.js</Title>
+          <Title>🍹 juice.js</Title>
         </Logo>
-        <Link href={DOCS_URL}>DOCSsss</Link>
+        <Link href={DOCS_URL}>DOCS</Link>
         <Link href={GITHUB_URL}>GITHUB</Link>
       </Header>
       <Main>
         <ControlPanel>
-          <Drop
-            onClick={(value) => {
-              const getValue = juice[value]
-              setJuicers(juicers.concat[{ getValue }])
+          <div
+            onClick={() => {
+              reset()
             }}
-            header={'select feature'}
-            options={features.map(([parameterName]) => ({
-              label: parameterName,
-              value: parameterName,
-            }))}
-          />
-          {/* {features.map(([key, feature]) => {
-            console.log('App -> feature', feature)
-            return (
-              <React.Fragment key={key}>
-                <div>{key}</div>
-                <div>
-                  {Object.entries(feature.parameters).map(
-                    ([parameterName, { value }]) => {
-                      return <div key={parameterName}>{parameterName}</div>
-                    },
-                  )}
-                </div>
-              </React.Fragment>
-            )
-          })} */}
+          >
+            Refresh
+          </div>
+          <ParameterTitle>Select feature</ParameterTitle>
+          <Options>
+            {features.map(({ name }) => {
+              return (
+                <Option
+                  onClick={() => {
+                    const feature = features.find(
+                      (feature) => feature.name === name,
+                    )
+                    const getValue = juice[name](
+                      _.mapValues('value')(feature.parameters),
+                    )
+                    reset()
+                    setJuicers([{ getValue }])
+                    setSelectedFeature(name)
+                  }}
+                  key={name}
+                  selected={selectedFeature === name}
+                >
+                  {name}
+                </Option>
+              )
+            })}
+          </Options>
+          {juicers.length > 0 ? (
+            <>
+              {_.map.convert({ cap: false })(({ value, optional }, key) => {
+                return (
+                  <React.Fragment key={key}>
+                    <p>
+                      {key} {optional ? '(optional)' : ''}
+                    </p>
+                    <input
+                      placeholder={value}
+                      onChange={({ target: { value: newValue } }) => {
+                        const feature = features.find(
+                          (feature) => feature.name === selectedFeature,
+                        )
+
+                        const configuration = _.mapValues.convert({
+                          cap: false,
+                        })((parameterValue, parameterKey) => {
+                          if (parameterKey === key) {
+                            if (!newValue) {
+                              return value
+                            }
+                            return newValue
+                          }
+                          return parameterValue.value
+                        })(feature.parameters)
+
+                        const getValue = juice[selectedFeature](configuration)
+                        reset()
+                        setJuicers([{ getValue }])
+                      }}
+                    />
+                  </React.Fragment>
+                )
+              })(
+                features.find((feature) => feature.name === selectedFeature)
+                  .parameters,
+              )}
+            </>
+          ) : null}
         </ControlPanel>
         <Canvas id="canvas"></Canvas>
       </Main>
-    </div>
+    </Container>
   )
 }
 
--- showcase2/src/constant/features.js
+++ showcase2/src/constant/features.js
@@ -1,65 +1,54 @@
 const features = [
-  [
-    'easeIn',
-    {
-      parameters: {
-        startValue: { value: 100, optional: false },
-        endValue: { value: 800, optional: false },
-        duration: { value: 180, optional: false },
-      },
+  {
+    name: 'easeIn',
+    parameters: {
+      duration: { value: 180, optional: false },
+      startValue: { value: 100, optional: false },
+      endValue: { value: 800, optional: false },
     },
-  ],
-  [
-    'easeInOut',
-    {
-      parameters: {
-        startValue: { value: 100, optional: false },
-        endValue: { value: 600, optional: false },
-        duration: { value: 120, optional: false },
-        startTime: { value: 0, optional: true },
-      },
+  },
+  {
+    name: 'easeInOut',
+    parameters: {
+      duration: { value: 120, optional: false },
+      startValue: { value: 100, optional: false },
+      endValue: { value: 600, optional: false },
+      startTime: { value: 0, optional: true },
     },
-  ],
-  [
-    'easeOut',
-    {
-      parameters: {
-        startValue: { value: 100, optional: false },
-        endValue: { value: 800, optional: false },
-        duration: { value: 180, optional: false },
-      },
+  },
+  {
+    name: 'easeOut',
+    parameters: {
+      duration: { value: 180, optional: false },
+      startValue: { value: 100, optional: false },
+      endValue: { value: 800, optional: false },
     },
-  ],
-  [
-    'linear',
-    {
-      parameters: {
-        startValue: { value: 100, optional: true },
-        speed: { value: 3, optional: true },
-      },
+  },
+  {
+    name: 'linear',
+    parameters: {
+      duration: { value: 120, optional: false },
+      endValue: { value: 300, optional: false },
+      startValue: { value: 100, optional: true },
     },
-  ],
-  [
-    'parabola',
-    {
-      parameters: {
-        duration: { value: 400, optional: false },
-        height: { value: 500, optional: false },
-        startValue: { value: 0, optional: true },
-        startTime: { value: 0, optional: true },
-      },
+  },
+  {
+    name: 'parabola',
+    parameters: {
+      duration: { value: 400, optional: false },
+      height: { value: 500, optional: false },
+      startValue: { value: 0, optional: true },
+      startTime: { value: 0, optional: true },
     },
-  ],
-  [
-    'sine',
-    {
-      parameters: {
-        startValue: { value: 100, optional: false },
-        endValue: { value: 700, optional: false },
-        duration: { value: 180, optional: false },
-      },
+  },
+  {
+    name: 'sine',
+    parameters: {
+      duration: { value: 180, optional: false },
+      startValue: { value: 100, optional: false },
+      endValue: { value: 700, optional: false },
     },
-  ],
+  },
 ]
 
 export default features
